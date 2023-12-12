4226d7ac30af2daadbb8225b0c2b2be8b607e6bc
rexbrandy <bailey.sdev@gmail.com>
2023-12-12 16:19:27 +0100

    added stuff

--- Games/ultimate_tic_tac_toe/ultimate_tic_tac_toe.py
+++ Games/ultimate_tic_tac_toe/ultimate_tic_tac_toe.py
@@ -1,8 +1,15 @@
 def get_board():
-    mini_board = [ [ None for a in range(3)] for b in range(3) ]
+    mini_board = [ [ a*b for a in range(3)] for b in range(3) ]
     board = [ [mini_board for a in range(3)] for b in range(3) ]
     
     return board
 
 def print_board(board):
-    
+    for mini in board:
+        for _ in range(3):
+            print(mini[0][_], mini[1][_], mini[2][_])
+            print(_)
+        print('_'*50)
+
+board = get_board()
+print_board(board)
